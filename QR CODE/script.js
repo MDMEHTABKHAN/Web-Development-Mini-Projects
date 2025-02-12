2bc05d27f3f6828293cabffbf3b5f2fc72d6e718
Md Mehtab Khan <mdmehtabkhan179@gmail.com>
2025-02-12 23:00:56 +0530

    first commit

---
+++ QR CODE/script.js
@@ -0,0 +1,43 @@
+let body = document.querySelector("body");
+document.body.style.margin = "0";
+document.body.style.padding = "0";
+document.body.style.boxSizing = "border-box";
+document.body.style.fontFamily = '"Outfit", sans-serif';
+document.body.style.fontOpticalSizing = "auto";
+document.body.style.fontWeight = "400";
+document.body.style.fontStyle = "normal";
+
+let bodyele = document.querySelector('body');
+bodyele.style.backgroundColor = "hsl(212, 45%, 89%)";
+bodyele.style.fontFamily = "outfit";
+bodyele.style.height = "100vh";
+bodyele.style.display = "flex";
+bodyele.style.flexDirection = "column";
+bodyele.style.alignItems = "center";
+bodyele.style.justifyContent = "center";
+
+let qrEle = document.querySelector(".qr-component");
+qrEle.style.width = "270px";
+qrEle.style.padding = "20px";
+qrEle.style.backgroundColor = "hsl(0, 0%, 100%)";
+qrEle.style.borderRadius = "25px";
+qrEle.style.fontSize = "15px";
+qrEle.style.textAlign = "center";
+qrEle.style.marginBottom = "30px";
+
+let imgele = document.querySelector(".img");
+imgele.style.width = "100%";
+imgele.style.borderRadius = "10px";
+imgele.style.textAlign = "center";
+imgele.style.height = "auto";
+
+let headele = document.querySelector(".head");
+headele.style.fontWeight = "800";
+headele.style.color = "hsl(218, 44%, 22%)";
+headele.style.textAlign = "center";
+headele.style.fontSize = "20px";
+headele.margin = "10px 0"
+
+let sectionele = document.querySelector(".text-section");
+sectionele.style.color = "hsl(220, 15%, 55%)";
+sectionele.textAlign = "center";
